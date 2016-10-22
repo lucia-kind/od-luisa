22ecca249aed8aec9f17bef64a7242a46e221849
IsabelleSchwab <belle_schwab@hotmail.com>
2016-10-22 12:51:45 +0200

    home gelöscht

--- index.php
+++ index.php
@@ -5,89 +5,95 @@
     $result = get_dogs_name();
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
+    <!DOCTYPE html>
+    <html>
 
-  <title>Hundenamen</title>
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <link rel="stylesheet" href="css/style.css">
+    <head>
 
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+        <title>Hundenamen</title>
 
-</head>
-<body>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <link rel="stylesheet" href="css/style.css">
+
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+
+    </head>
+
+    <body>
 
 
 
-  <div id="demo">
-    <ul>
-        <?php while ($dog = mysqli_fetch_assoc($result)) { ?>
-         
-         
-         <? $newVar[] = $dog['hundename']; ?>
-         
-          <li class="dragend-page">
-            <p id="<?php echo $dog['id']?>"><?php echo $dog['hundename']; ?></p>
-            <p>0.01% in Z&uuml;rich</p>
-            <p>99 Herzen</p>
-            <p>1/19</p>
-          </li>
-        <? } ?>
-    </ul>
+        <div id="demo">
+            <ul>
+                <?php while ($dog = mysqli_fetch_assoc($result)) { ?>
 
-  </div>
-    
+
+                    <? $newVar[] = $dog['hundename']; ?>
+
+                        <li class="dragend-page">
+                            <p id="<?php echo $dog['id']?>">
+                                <?php echo $dog['hundename']; ?>
+                            </p>
+                            <p>0.01% in Z&uuml;rich</p>
+                            <p>99 Herzen</p>
+                            <p>1/19</p>
+                        </li>
+                        <? } ?>
+            </ul>
+
+        </div>
+
         <pre>
             <?php $newVar = json_encode($newVar); ?>            
-        </pre>    
-    
-  <script type="text/javascript" src="js/jquery.min.js"></script>
-  <script type="text/javascript" src="js/dragend.js"></script>
+        </pre>
 
-  <script>
-      
-    //var names = <?php echo $newVar ?>;
-    //nur id übergeben? als klasse? oder id?!
-      var names = [];
-      
-    function onSwipe () {
-        if(Right) {
-            names.push('gewswipter Name');            
-        }
-        //ul li delete gswipter name
-    }
-      
-    function onSubmit () {
-        form.hiddenName.value = names;
-    }
-      
-    
-    //localStorage.setItem("names", names);
-    
-    console.log(names);
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/dragend.js"></script>
 
-    $(function() {
-      new Dragend($("#demo").get(0), {
-        afterInitialize: function() {
-          $("#demo").css("visibility", "visible");
-        }
-      });
-    });
+        <script>
+            //var names = <?php echo $newVar ?>;
+            //nur id übergeben? als klasse? oder id?!
+            var names = [];
 
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-219062-10']);
-    _gaq.push(['_trackPageview']);
+            function onSwipe() {
+                if (Right) {
+                    names.push('gewswipter Name');
+                }
+                //ul li delete gswipter name
+            }
 
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+            function onSubmit() {
+                form.hiddenName.value = names;
+            }
 
-  </script>
 
-</body>
-</html>
+            //localStorage.setItem("names", names);
+
+            console.log(names);
+
+            $(function () {
+                new Dragend($("#demo").get(0), {
+                    afterInitialize: function () {
+                        $("#demo").css("visibility", "visible");
+                    }
+                });
+            });
+
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-219062-10']);
+            _gaq.push(['_trackPageview']);
+
+            (function () {
+                var ga = document.createElement('script');
+                ga.type = 'text/javascript';
+                ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
+
+    </body>
+
+    </html>
