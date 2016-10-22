b4a6d4a637bc7fe2619c2e868efaf797e60d0207
IsabelleSchwab <belle_schwab@hotmail.com>
2016-10-22 21:53:55 +0200

    :(
    Namenstinder; erkennt getelementbyClassName(‚visible‘).id nicht…. wieso?

--- index.php
+++ index.php
@@ -3,91 +3,121 @@
 	require_once("system/security.php");
 
     $result = get_dogs_name();
+    //$row = mysqli_fetch_row ($result);
+    
 ?>
+    <!DOCTYPE html>
+    <html>
 
-<!DOCTYPE html>
-<html>
-<head>
+    <head>
 
-  <title>Hundenamen</title>
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <link rel="stylesheet" href="css/style.css">
+        <title>Hundenamen</title>
+        <meta charset="utf-8">
 
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <link rel="stylesheet" href="css/style.css">
 
-</head>
-<body>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
 
-    
-    
-  <div id="demo">
-    <ul>
-        <?php while ($dog = mysqli_fetch_assoc($result)) { ?>
-         
-         
-         <? $newVar[] = $dog['hundename']; ?>
-         
-          <li class="dragend-page">
-            <p id="<?php echo $dog['id']?>"><?php echo $dog['hundename']; ?></p>
-            <p>0.01% in Z&uuml;rich</p>
-            <p>99 Herzen</p>
-            <p>1/19</p>
-          </li>
-        <? } ?>
-    </ul>
+    </head>
 
-  </div>
-    
+    <body>
+
+        <div id="demo2">
+            <!--versuch mit invisible-->
+            <div class="container">
+
+                Name:
+
+                <?php while ($dog = mysqli_fetch_assoc($result)) { ?>
+                    <!--                   hier werden 20 Namen aufgelistet-->
+                    <div id="<?php echo $dog['id']?>" class="dogname">
+
+                        <?php echo $dog['hundename'];
+                        $newVar = $dog['hundename'];
+                        ?>
+
+                    </div>
+                    <?php } 
+                        ?>
+
+
+            </div>
+            <button type="button" id="button">click me </button>
+            <!--invisible versuch ende-->
+            <button onclick="like()">LIKE</button>
+            <button onclick="dislike()">DISLIKE</button>
+
+        </div>
+
         <pre>
-            <?php $newVar = json_encode($newVar); ?>            
-        </pre>    
-    
-  <script type="text/javascript" src="js/jquery.min.js"></script>
-  <script type="text/javascript" src="js/dragend.js"></script>
+            <?php $newVar = json_encode($newVar);?> <!--damit js das lesen kann-->  
+        </pre>
 
-  <script>
-      
-    //var names = <?php echo $newVar ?>;
-    //nur id übergeben? als klasse? oder id?!
-      var names = [];
-      
-    function onSwipe () {
-        if(Right) {
-            names.push('gewswipter Name');            
-        }
-        //ul li delete gswipter name
-    }
-      
-    function onSubmit () {
-        form.hiddenName.value = names;
-    }
-      
-    
-    //localStorage.setItem("names", names);
-    
-    console.log(names);
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/onSwipe.js"></script>
+        <script type="text/javascript" src="js/load.js"></script>
 
-    $(function() {
-      new Dragend($("#demo").get(0), {
-        afterInitialize: function() {
-          $("#demo").css("visibility", "visible");
-        }
-      });
-    });
+        <script>
+            var names = <?php echo $newVar ?>; //der aktuelle Name
+            //achtung, später nur id übergeben
 
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-219062-10']);
-    _gaq.push(['_trackPageview']);
+            var disliked = [];
+            var liked = [];
+            var gezeigt = [];
 
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
 
-  </script>
+            function like() {
+                liked.push('<?php echo $newVar ?>');
+                gezeigt.push('<?php echo $newVar ?>');
+                console.log(liked);
+            }
 
-</body>
-</html>
+            function dislike() {
+                disliked.push('<?php echo $newVar ?>');
+                gezeigt.push('<?php echo $newVar ?>');
+                console.log(disliked);
+
+            }
+
+            function onSwipe() {
+                if (Right) {
+                    names.push('gewswipter Name');
+                }
+                //ul li delete gswipter name
+            }
+
+            function onSubmit() { //Wenn auf Duell gewechselt wird
+                form.hiddenName.value = names;
+            }
+
+            localStorage.setItem("names", names); //speichere aktuelle Namen
+            console.log(names);
+        </script>
+        <script>
+            $(document).ready(function () {
+
+                // mache alle unsichtbar ausser das erste
+                $('.dogname').not(':first').addClass("invisible");
+                $('.dogname').first().addClass("visible"); //gibt dem ersten die class visible
+
+            });
+
+            $(function () { //zeige den nächsten namen in der Liste an
+                $("#button").click(function () {
+                    $("div.container div.invisible").first().addClass("visible").removeClass("invisible");
+                    $("div.container div.visible").first().addClass("shown").removeClass("visible");
+                });
+            });
+
+            //name speichern für später
+            $(document).ready(function () {
+                document.getElementsByClassName('.visible').style.color = "blue"; //greift nicht, wieso?
+                console.log("aktueller Name:" + aktuellerName);
+            });
+        </script>
+
+    </body>
+
+    </html>
