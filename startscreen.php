47e43ecb1a111607881f71e223fdadcd90d2b647
IsabelleSchwab <belle_schwab@hotmail.com>
2016-10-24 17:42:39 +0200

    Verwende jetzt javascript array statt php array
    ES FUNKTIONIERT

--- startscreen.php
+++ startscreen.php
@@ -5,176 +5,200 @@
     $result = get_dogs_name();
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
+    <!DOCTYPE html>
+    <html>
 
-  <title>Luisa - Die Namensapp</title>
-    
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/animations.css">
-    
-    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <head>
 
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+        <title>Luisa - Die Namensapp</title>
 
-</head>
-<body>
-    
-  <div  class="section">
-    <div class="topbar">
-        <div class="txt_topbar">Babyname</div>
-    </div> <!-- topbar -->
-    <div class="name_tinder">
-        <div id="demo slider">
-             <div class="container">  
-                <ul>
-            
-                Name:
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-                <?php 
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/animations.css">
+
+        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+
+    </head>
+
+    <body>
+
+        <div class="section">
+            <div class="topbar">
+                <div class="txt_topbar">Babyname</div>
+            </div>
+            <!-- topbar -->
+            <div class="name_tinder">
+                <div id="demo slider">
+                    <div class="container">
+                        <ul>
+
+                            Name:
+
+                            <?php 
                   $array = [];//leerer Array für 20 Namen
                 while ($dog = mysqli_fetch_assoc($result)) { ?>
-                    <!-- hier werden 20 Namen aufgelistet-->
-                    <div id="<?php echo $dog['id']?>" class="dogname">
-                        <?php $newVar = $dog['hundename'];
+                                <!-- hier werden 20 Namen aufgelistet-->
+                                <div id="<?php echo $dog['id']?>" class="dogname">
+                                    <?php $newVar = $dog['hundename'];
                         array_push($array, $dog['hundename']);
                             //und im Array gespeichert
                                    echo $dog['hundename'];
                              //wird angezeigt
                         ?>
 
-                    </div>
-                    <?php } 
+                                </div>
+                                <?php } 
                         ?>
-                </ul>
+                        </ul>
 
-       
-            <button type="button" id="button">click me </button>
-            <!--invisible versuch ende-->
-            <button onclick="like();">LIKE</button>
-            <button onclick="dislike();">DISLIKE</button>
 
-        
-         
-             </div> <!-- container -->
-        </div> <!-- demo -->
-    </div> <!-- name_tinder -->
-    <div class="hearts">
-        <div class="bheart"><img src="img/bheart.png"/></div>
-        <div class="bheart"><img src="img/heart.png"/></div>
-        <div class="clear"></div>
-    </div> <!-- hearts -->
-    <div class="menu">
-        <div class="duell"><a href="registrieren.php">Duell!</a></div>
-      	<section id="actions">
+                        <!--<button type="button" id="button">click me </button>-->
+                        <!--invisible versuch ende-->
+                        <button onclick="like();">LIKE</button>
+                        <button onclick="dislike();">DISLIKE</button>
 
-		<div class="container einstellungen">
-			<a href="#" id="slideUpBtn" class="button">Einstellungen</a>
-		</div>
 
-	</section>
-    </div> <!-- menu -->
-  </div> <!-- section --> 
-    
+
+                    </div>
+                    <!-- container -->
+                </div>
+                <!-- demo -->
+            </div>
+            <!-- name_tinder -->
+            <div class="hearts">
+                <div class="bheart"><img src="img/bheart.png" /></div>
+                <div class="bheart"><img src="img/heart.png" /></div>
+                <div class="clear"></div>
+            </div>
+            <!-- hearts -->
+            <div class="menu">
+                <div class="duell"><a href="registrieren.php">Duell!</a></div>
+                <section id="actions">
+
+                    <div class="container einstellungen">
+                        <a href="#" id="slideUpBtn" class="button">Einstellungen</a>
+                    </div>
+
+                </section>
+            </div>
+            <!-- menu -->
+        </div>
+        <!-- section -->
+
         <pre>
             <?php
                print_r($array);?><!--letzter?-->
              <!--damit js das lesen kann-->  
         </pre>
-    
-<!-- STOP FILTER TIME --> 
-    <section id="animation">
 
-		<div class="animation-container">
-            <div id="object" class="animate tossing">
-                <div class="section">
-                    <div class="topbar">
-                        <div class="txt_topbar">Einstellungen</div>
-                    </div>
+        <!-- STOP FILTER TIME -->
+        <section id="animation">
 
-                    <div class="formline">
-                        <input type="text" placeholder="Nachname" id="vname" name="vname">
-                    </div>
+            <div class="animation-container">
+                <div id="object" class="animate tossing">
+                    <div class="section">
+                        <div class="topbar">
+                            <div class="txt_topbar">Einstellungen</div>
+                        </div>
 
-                    <div class="formline">
-                        <input type="text" placeholder="Anfangsbuchstabe" id="abst" name="abuchstabe">
-                    </div>
+                        <div class="formline">
+                            <input type="text" placeholder="Nachname" id="vname" name="vname">
+                        </div>
 
-                    <div class="formline">
-                         <input type="checkbox" id="mw" name="alter"> m
-                         <input type="checkbox" id="wm" name="alter"> w
-                    </div>
+                        <div class="formline">
+                            <input type="text" placeholder="Anfangsbuchstabe" id="abst" name="abuchstabe">
+                        </div>
 
-                    <div class="formline">
-                        Maximal <input type="text" placeholder="x" id="minimaxi" name="maxi"> Zeichen
-                    </div>
+                        <div class="formline">
+                            <input type="checkbox" id="mw" name="alter"> m
+                            <input type="checkbox" id="wm" name="alter"> w
+                        </div>
 
-                    <div class="formline">
-                        Minimal <input type="text" placeholder="x" id="minimaxi" name="mini"> Zeichen
-                    </div>
+                        <div class="formline">
+                            Maximal
+                            <input type="text" placeholder="x" id="minimaxi" name="maxi"> Zeichen
+                        </div>
 
-                    <div class="formline">
+                        <div class="formline">
+                            Minimal
+                            <input type="text" placeholder="x" id="minimaxi" name="mini"> Zeichen
+                        </div>
 
-                        <select id="dropdown"name="Häufigkeit">
-                            <option value="häufigkeit">Häufigkeit</option>
-                            <option value="sehr beliebt">Sehr beliebt</option>
-                            <option value="beliebt">Beliebt</option>
-                            <option value="selten">Selten</option>
-                            <option value="sehr selten">Sehr selten</option>
-                        </select>
+                        <div class="formline">
 
-                    </div>
+                            <select id="dropdown" name="Häufigkeit">
+                                <option value="häufigkeit">Häufigkeit</option>
+                                <option value="sehr beliebt">Sehr beliebt</option>
+                                <option value="beliebt">Beliebt</option>
+                                <option value="selten">Selten</option>
+                                <option value="sehr selten">Sehr selten</option>
+                            </select>
 
-                    <div class="formline">
-                    </div>
+                        </div>
 
-                    <div class="formline">
-                    </div>
+                        <div class="formline">
+                        </div>
 
-                    <div class="tickbox">
-                        <div class="container einstellungen">
-                        <a href="#" id="slideDownBtn" class="button">Slide Down &#8595;</a>
+                        <div class="formline">
+                        </div>
+
+                        <div class="tickbox">
+                            <div class="container einstellungen">
+                                <a href="#" id="slideDownBtn" class="button">Slide Down &#8595;</a>
+                            </div>
                         </div>
                     </div>
-                </div> <!-- section -->
-            
+                    <!-- section -->
+
+                </div>
+
             </div>
-            
-		</div>
 
 
-	</section>
+        </section>
 
-        
 
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/onSwipe.js"></script>
-    <script type="text/javascript" src="js/load.js"></script>
-        
-    <!-- Isa CODE-->
-            <script>
+
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/onSwipe.js"></script>
+        <script type="text/javascript" src="js/load.js"></script>
+
+        <!-- TINDER CODE-->
+        <script>
             //der aktuelle Name
             //achtung, später nur id übergeben
+            var namensArray = <?php echo json_encode($array); ?>;
+            console.log(namensArray);
 
             var disliked = [];
             var liked = [];
 
             //Tinderfunktionen
             function like() {
-                liked.push("<?php echo array_shift($array); ?>");
-                //wendet es so oder so an? why?
+                liked.push(namensArray[0]);
+                namensArray.shift();
+
                 console.log("liked:" + liked);
+                //nächster Name
+                $("div.container div.invisible").first().addClass("visible").removeClass("invisible");
+                $("div.container div.visible").first().addClass("shown").removeClass("visible");
+                document.getElementsByClassName('visible')[0].style.color = "blue";
             }
 
             function dislike() {
-                disliked.push("<?php echo array_shift($array); ?>");
-                //wendet es so oder so an? why?
+                disliked.push(namensArray[0]);
+                namensArray.shift();
+
                 console.log("disliked:" + disliked);
+                //nächster Name
+                $("div.container div.invisible").first().addClass("visible").removeClass("invisible");
+                $("div.container div.visible").first().addClass("shown").removeClass("visible");
+                document.getElementsByClassName('visible')[0].style.color = "blue";
 
             }
             //Tinderfunktionen Ende
@@ -203,48 +227,48 @@
                 document.getElementsByClassName('visible')[0].style.color = "blue";
             });
 
-            //zeige den nächsten namen in der Liste an
-            $(function () {
-                $("#button").click(function () {
-                    $("div.container div.invisible").first().addClass("visible").removeClass("invisible");
-                    $("div.container div.visible").first().addClass("shown").removeClass("visible");
+            /*zeige den nächsten namen in der Liste an
+                        $(function () {
+                            $("#button").click(function () {
+                               $("div.container div.invisible").first().addClass("visible").removeClass("invisible");
+                                $("div.container div.visible").first().addClass("shown").removeClass("visible");
+            
+                            });
+                        });*/
+        </script>
+        <!--Tinder Ende -->
 
+
+        <!-- Slide Einstellungen / Filter -->
+        <script>
+            $(window).scroll(function () {
+                $('#examples-cta').each(function () {
+                    var imagePos = $(this).offset().top;
+
+                    var topOfWindow = $(window).scrollTop();
+                    if (imagePos < topOfWindow + 400) {
+                        $(this).addClass("slideUp, slideDown");
+                    }
+                });
+            });
+
+            /* ENTRANCES */
+            $('#slideUpBtn').click(function () {
+                $(this).each(function () {
+                    $('#object').removeClass();
+                    $('#object').addClass("slideUp");
+                });
+            });
+
+            $('#slideDownBtn').click(function () {
+                $(this).each(function () {
+                    $('#object').removeClass();
+                    $('#object').addClass("slideDown");
                 });
             });
         </script>
-    
-    
-    <!-- Slide Einstellungen / Filter -->
-    <script>
-
-	$(window).scroll(function() {
-		$('#examples-cta').each(function(){
-		var imagePos = $(this).offset().top;
-		
-		var topOfWindow = $(window).scrollTop();
-			if (imagePos < topOfWindow+400) {
-				$(this).addClass("slideUp, slideDown");
-			}
-		});
-	});
-
-        /* ENTRANCES */
-		$('#slideUpBtn').click(function() {
-			$(this).each(function(){
-					$('#object').removeClass();	
-					$('#object').addClass("slideUp");
-				});
-		});
-        
-        $('#slideDownBtn').click(function() {
-			$(this).each(function(){
-					$('#object').removeClass();				
-					$('#object').addClass("slideDown");
-				});
-		});	
-        
-     </script>
 
 
-</body>
-</html>
+    </body>
+
+    </html>
