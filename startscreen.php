f7ada5848f81d2ac953e07add184a4dc80c7e34d
IsabelleSchwab <belle_schwab@hotmail.com>
2016-10-30 21:58:12 +0100

    Weiterarbeit Formular
    Problem: Submit Button und Radio Buttons

--- startscreen.php
+++ startscreen.php
@@ -1,39 +1,56 @@
 <?php
 	require_once("system/data.php");
 	require_once("system/security.php");
+	require_once("system/functions.php");
 
     $result = get_dogs_name();
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
+    <!DOCTYPE html>
+    <html>
 
-  <title>Luisa - Die Namensapp</title>
-    
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/animations.css">
-    
-    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <head>
 
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+        <title>Luisa - Die Namensapp</title>
 
-</head>
-<body>
-    
-  <div  class="section">
-    <div class="topbar">
-        <div class="txt_topbar">Babyname</div>
-    </div> <!-- topbar -->
-    <div class="name_tinder">
-        <div id="demo slider">
-            <div class="container">
-            <ul>
-                Name:
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/animations.css">
+
+        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+
+        <script>
+            //hier wird das Tinder initiert bzw nur ein name angezeigt
+            //muss am Anfang stehen
+            $(document).ready(function () {
+                // mache alle unsichtbar ausser das erste
+                $('.dogname').not(':first').addClass("invisible");
+                //gibt dem ersten die class visible
+                $('.dogname').first().addClass("visible");
+                //spreche den angezeigten über klasse namen an
+                document.getElementsByClassName('visible')[0].style.color = "blue";
+            });
+        </script>
+
+    </head>
+
+    <body>
+
+        <div class="section">
+            <div class="topbar">
+                <div class="txt_topbar">Babyname</div>
+            </div>
+            <!-- topbar -->
+            <div class="name_tinder">
+                <div id="demo slider">
+                    <div class="container">
+                        <ul>
+                            Name:
 
                             <?php 
                   $array = [];//leerer Array für 20 Namen
@@ -50,104 +67,113 @@
                                 </div>
                                 <?php } 
                         ?>
-            </ul>
-            </div> <!-- container -->
-        </div> <!-- demo -->
-    </div> <!-- name_tinder -->
-    <div class="hearts">
-                        <!--<button type="button" id="button">click me </button>-->
+                        </ul>
+                    </div>
+                    <!-- container -->
+                </div>
+                <!-- demo -->
+            </div>
+            <!-- name_tinder -->
+            <div class="hearts">
+                <!--<button type="button" id="button">click me </button>-->
                 <!--invisible versuch ende-->
                 <div class="bheart">
-                    <input type="image" src="img/heart.png" onclick="like();"/>
+                    <input type="image" src="img/heart.png" onclick="like();" />
                 </div>
                 <div class="bheart">
-                    <input type="image" src="img/bheart.png" onclick="dislike();"/>
+                    <input type="image" src="img/bheart.png" onclick="dislike();" />
                 </div>
-        <div class="clear"></div>
-    </div> <!-- hearts -->
-    <div class="menu">
-        <div class="duell"><a href="registrieren.php">Duell!</a></div>
-      	<section id="actions">
-
-		<div class="container einstellungen">
-			<a href="#" id="slideUpBtn" class="button">Einstellungen</a>
-		</div>
-
-	</section>
-    </div> <!-- menu -->
-  </div> <!-- section --> 
-       <!-- STOP FILTER TIME -->
-    	<section id="animation">
-
-		<div class="animation-container">
-			<div id="object" class="animate tossing">
-        
-        <div class="section">
-        
-        <div class="topbar">
-            <div class="txt_topbar">Einstellungen</div>
-        </div>
-        
-        <div class="formline">
-            <input type="text" placeholder="Nachname" id="vname" name="vname">
-        </div>
-        
-        <div class="formline">
-            <input type="text" placeholder="Anfangsbuchstabe" id="abst" name="abuchstabe">
-        </div>
-    
-        <div class="formline">
-             <input type="checkbox" id="mw" name="alter"> m
-             <input type="checkbox" id="wm" name="alter"> w
-        </div>
-    
-        <div class="formline">
-            Maximal <input type="text" placeholder="x" id="minimaxi" name="maxi"> Zeichen
-        </div>
-    
-        <div class="formline">
-            Minimal <input type="text" placeholder="x" id="minimaxi" name="mini"> Zeichen
-        </div>
-    
-        <div class="formline">
-            
-            <select id="dropdown"name="Häufigkeit">
-                <option value="häufigkeit">Häufigkeit</option>
-                <option value="sehr beliebt">Sehr beliebt</option>
-                <option value="beliebt">Beliebt</option>
-                <option value="selten">Selten</option>
-                <option value="sehr selten">Sehr selten</option>
-            </select>
-    
-        </div>
-    
-        <div class="formline">
-        </div>
-        <div class="formline">
-        </div>
-        
-        <div class="tickbox">
-            <input style="padding-top: 5%; height: 35%;"  type="image" src="img/tick.png" onclick="window.location.href='/startscreen.php'"/>
-        </div>
-    
-    </div> <!-- section -->
-            
+                <div class="clear"></div>
             </div>
-            
-		</div>
+            <!-- hearts -->
+            <div class="menu">
+                <div class="duell"><a href="registrieren.php">Duell!</a></div>
+                <section id="actions">
+
+                    <div class="container einstellungen">
+                        <a href="#" id="slideUpBtn" class="button">Einstellungen</a>
+                    </div>
+
+                </section>
+            </div>
+            <!-- menu -->
+        </div>
+        <!-- section -->
+        <!-- STOP FILTER TIME -->
+        <section id="animation">
+
+            <div class="animation-container">
+                <div id="object" class="animate tossing">
+                    <!--FORMULAR ANFANG-->
+                    <form method="post">
+
+                        <div class="section">
+
+                            <div class="topbar">
+                                <div class="txt_topbar">Einstellungen</div>
+                            </div>
+
+                            <div class="formline">
+                                <input type="text" placeholder="Nachname" id="vname" name="vname">
+                            </div>
+
+                            <div class="formline">
+                                <input type="text" placeholder="Anfangsbuchstabe" id="abst" name="abuchstabe">
+                            </div>
+
+                            <div class="formline">
+                                <input type="radio" id="m" name="geschlecht"> m
+                                <input type="radio" id="w" name="geschlecht"> w
+                            </div>
+
+                            <div class="formline">
+                                Maximal
+                                <input type="text" placeholder="x" id="minimaxi" name="maxi"> Zeichen
+                            </div>
+
+                            <div class="formline">
+                                Minimal
+                                <input type="text" placeholder="x" id="minimaxi" name="mini"> Zeichen
+                            </div>
+
+                            <div class="formline">
+
+                                <select id="dropdown" name="Häufigkeit">
+                                    <option value="häufigkeit">Häufigkeit</option>
+                                    <option value="sehr beliebt">Sehr beliebt</option>
+                                    <option value="beliebt">Beliebt</option>
+                                    <option value="selten">Selten</option>
+                                    <option value="sehr selten">Sehr selten</option>
+                                </select>
+
+                            </div>
+
+                            <div class="formline">
+                                <input type="submit" name="SubmitButton" />
+                                <!--geht nicht ohne-->
+                            </div>
+                            <div class="formline">
+                            </div>
+
+                            <div id="filter" class="tickbox">
+                                <input style="padding-top: 5%; height: 35%;" type="image" src="img/tick.png" onclick="window.location.href='/startscreen.php';" />
+                            </div>
+
+                        </div>
+                        <!-- section -->
+                    </form>
+                    <!--FORM ENDE -->
+
+                </div>
+
+            </div>
 
 
-	</section>
+        </section>
 
-        
-        <pre>
-            <?php $newVar = json_encode($newVar); ?>
-                 
-        </pre> 
-    
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    
-    <!-- TINDER CODE-->
+
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <!-- TINDER CODE-->
         <script>
             //der aktuelle Name
             //achtung, später nur id übergeben
@@ -194,52 +220,45 @@
 
             //übergang zum Duell Ende
             console.log("<?php echo $array[0]; ?>"); //der erste name des Arrays
+
+
+
+            //<!-- test post form-->
+
+            var haufen = <?php echo json_encode($hauf); ?>;
+            var nachname = <?php echo json_encode($nachname); ?>;
+            var max = <?php echo json_encode($max); ?>;
+            var min = <?php echo json_encode($min); ?>;
+            var anfang = <?php echo json_encode($anfang); ?>;
+            var geschlecht = <?php echo json_encode($geschlecht); ?>;
+            console.log("Haüfigkeit: " + haufen + "min: " + min + "max: " + max + "anfang:" + anfang + "nachname: " + nachname + "geschlecht:" + geschlecht);
         </script>
+
+        <!--Tinder Ende -->
+        <!--filter hoch und runter-->
+
         <script>
-            //hier wird das Tinder initiert bzw nur ein name angezeigt
-            $(document).ready(function () {
-                // mache alle unsichtbar ausser das erste
-                $('.dogname').not(':first').addClass("invisible");
-                //gibt dem ersten die class visible
-                $('.dogname').first().addClass("visible");
-                //spreche den angezeigten über klasse namen an
-                document.getElementsByClassName('visible')[0].style.color = "blue";
+            $(window).scroll(function () {
+                $('#examples-cta').each(function () {
+                    var imagePos = $(this).offset().top;
+
+                    var topOfWindow = $(window).scrollTop();
+                    if (imagePos < topOfWindow + 400) {
+                        $(this).addClass("slideUp");
+                    }
+                });
             });
 
-            /*zeige den nächsten namen in der Liste an
-                        $(function () {
-                            $("#button").click(function () {
-                               $("div.container div.invisible").first().addClass("visible").removeClass("invisible");
-                                $("div.container div.visible").first().addClass("shown").removeClass("visible");
-            
-                            });
-                        });*/
+            /* ENTRANCES */
+            $('#slideUpBtn').click(function () {
+                $(this).each(function () {
+                    $('#object').removeClass();
+                    $('#object').addClass("slideUp");
+                });
+            });
         </script>
-        <!--Tinder Ende -->
-    
-    <script>
-
-	$(window).scroll(function() {
-		$('#examples-cta').each(function(){
-		var imagePos = $(this).offset().top;
-		
-		var topOfWindow = $(window).scrollTop();
-			if (imagePos < topOfWindow+400) {
-				$(this).addClass("slideUp");
-			}
-		});
-	});
-
-/* ENTRANCES */
-		$('#slideUpBtn').click(function() {
-			$(this).each(function(){
-					$('#object').removeClass();	
-					$('#object').addClass("slideUp");
-				});
-		});
-
-</script>
 
 
-</body>
-</html>
+    </body>
+
+    </html>
---
+++ system/functions.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_POST['SubmitButton'])){ //check if form was submitted
+    $hauf = $_POST['Häufigkeit'];
+    $anfang = $_POST["abuchstabe"];
+    $nachname = $_POST["vname"];
+    $max = $_POST["maxi"];
+    $min = $_POST["mini"];
+    $geschlecht = $_POST["geschlecht"];
+}    
+?>
