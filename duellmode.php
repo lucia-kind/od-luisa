15d9b065df559562e5da85e004f87a43ac989834
IsabelleSchwab <belle_schwab@hotmail.com>
2016-11-02 11:08:30 +0100

    Duell: Namen werden richtig angezeigt

--- duellmode.php
+++ duellmode.php
@@ -5,32 +5,6 @@
 
     <!DOCTYPE html>
     <html>
-
-    <head>
-
-        <title>Luisa - Die Namensapp</title>
-
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-        <link rel="stylesheet" href="css/style.css">
-
-        <div class="section">
-            <div class="topbar">
-                <div class="txt_topbar">Duell</div>
-            </div>
-            <div class="select_duellone">
-                <div id="name01" class="txt_one">Lorano</div>
-                <!--noch austauschen-->
-            </div>
-            <div class="select_duelltwo">
-                <div id="name02" class="txt_two">WauWau</div>
-                <!--noch austauschen-->
-            </div>
-        </div>
-
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-        <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
-
-    </head>
     <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
     <script>
         var duell = localStorage.getItem('liked');
@@ -39,7 +13,9 @@
         console.log(duell_array.length);
         $(document).ready(function () {
             var i = 0;
-            var laenge = duell_array.length;
+            var laenge = duell_array.length - 2;
+            document.getElementById('name01').innerHTML = duell_array.shift();
+            document.getElementById('name02').innerHTML = duell_array.shift();
 
             document.getElementById('name01').onclick = function () {
                 if (i < laenge) {
@@ -60,6 +36,37 @@
             }
         });
     </script>
+
+    <head>
+
+        <title>Luisa - Die Namensapp</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <link rel="stylesheet" href="css/style.css">
+
+        <div class="section">
+            <div class="topbar">
+                <div class="txt_topbar">Duell</div>
+            </div>
+            <div class="select_duellone">
+                <div id="name01" class="txt_one">
+                    Name 01
+                </div>
+                <!--noch austauschen-->
+            </div>
+            <div class="select_duelltwo">
+                <div id="name02" class="txt_two">
+                    Name 02
+                </div>
+                <!--noch austauschen-->
+            </div>
+        </div>
+
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+
+    </head>
+
 
     <body>
 
