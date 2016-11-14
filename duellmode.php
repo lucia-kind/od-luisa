009bd51178d1ba67a7628a544def2a98021236df
IsabelleSchwab <belle_schwab@hotmail.com>
2016-11-14 16:33:29 +0100

    win und Weitergabe funktioniert

--- duellmode.php
+++ duellmode.php
@@ -11,7 +11,7 @@
         
         session_start(); 
     if (isset($_SESSION["currentUser"])) { 
-        
+        $winner = "empty";
         $duell_id = $_GET["duell_id"];
         $sql = "SELECT namensliste FROM duell WHERE id = $duell_id;";
         $duell = get_result($sql)->fetch_array();
@@ -19,89 +19,94 @@
         if (isset($_POST['winner'])) {
             $winner = $_POST['winner']; 
             $query = "INSERT INTO wins (win_name, duell_id) VALUES ('$winner', $duell_id);";
-            get_result($query);
-            print_r($query);
+            get_result($query); ?>
+        <script>
+            window.location.href = 'win.php?name=<?php echo $winner; ?>';
+        </script>
+
+        <?php
         }
 
         
         ?>
-        <script>
-            //hole die gelikeden namen aus der Datenbank
-            var duell_array = <?php print_r($duell['namensliste']); ?>;
-            //Kontrolle
-            console.log("Duell:" + duell_array);
+            <script>
+                //hole die gelikeden namen aus der Datenbank
+                var duell_array = <?php print_r($duell['namensliste']); ?>;
+                //Kontrolle
+                console.log("Duell:" + duell_array);
 
-            $(document).ready(function () {
-                var i = 0;
-                var laenge = duell_array.length - 2;
-                document.getElementById('name01').innerHTML = duell_array.shift();
-                document.getElementById('name02').innerHTML = duell_array.shift();
+                $(document).ready(function () {
+                    var i = 0;
+                    var laenge = duell_array.length - 2;
+                    document.getElementById('name01').innerHTML = duell_array.shift();
+                    document.getElementById('name02').innerHTML = duell_array.shift();
 
-                document.getElementById('name01').onclick = function () {
-                    if (i < laenge) {
-                        document.getElementById('name02').innerHTML = duell_array.shift();
-                        i++;
-                    } else {
-                        var winner = document.getElementById('name01').innerHTML;
-                        console.log(winner);
-                        document.getElementById("hidden_winner").value = winner;
-                        document.getElementById("hiddenForm").submit();
+                    document.getElementById('name01').onclick = function () {
+                        if (i < laenge) {
+                            document.getElementById('name02').innerHTML = duell_array.shift();
+                            i++;
+                        } else {
+                            var winner = document.getElementById('name01').innerHTML;
+                            console.log(winner);
+                            document.getElementById("hidden_winner").value = winner;
+                            document.getElementById("hiddenForm").submit();
+                        }
                     }
-                }
-                document.getElementById('name02').onclick = function () {
-                    if (i < laenge) {
-                        document.getElementById('name01').innerHTML = duell_array.shift();
-                        i++;
-                    } else {
-                        var winner = document.getElementById('name02').innerHTML;
-                        console.log(winner);
-                        document.getElementById("hidden_winner").value = winner;
-                        document.getElementById("hiddenForm").submit();
-                        //document.hiddenForm.submit();
-                        //speichere in hidden field submit und dann in db
+                    document.getElementById('name02').onclick = function () {
+                        if (i < laenge) {
+                            document.getElementById('name01').innerHTML = duell_array.shift();
+                            i++;
+                        } else {
+                            var winner = document.getElementById('name02').innerHTML;
+                            console.log(winner);
+                            document.getElementById("hidden_winner").value = winner;
+                            document.getElementById("hiddenForm").submit();
+                            //document.hiddenForm.submit();
+                            //speichere in hidden field submit und dann in db
+
+
+                        }
 
                     }
+                });
+            </script>
 
-                }
-            });
-        </script>
+            <head>
+                <title>Luisa - Die Namensapp</title>
 
-        <head>
-            <title>Luisa - Die Namensapp</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+                <link rel="stylesheet" href="css/style.css">
+            </head>
 
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-            <link rel="stylesheet" href="css/style.css">
-        </head>
-
-        <body>
-            <div class="section">
-                <div class="topbar">
-                    <div class="txt_topbar">Duell</div>
-                </div>
-                <div class="select_duellone">
-                    <div id="name01" class="txt_one">
-                        Name 01
+            <body>
+                <div class="section">
+                    <div class="topbar">
+                        <div class="txt_topbar">Duell</div>
                     </div>
-                    <!--noch austauschen-->
-                </div>
-                <div class="select_duelltwo">
-                    <div id="name02" class="txt_two">
-                        Name 02
+                    <div class="select_duellone">
+                        <div id="name01" class="txt_one">
+                            Name 01
+                        </div>
+                        <!--noch austauschen-->
                     </div>
-                    <form id="hiddenForm" method="post">
-                        <input type="hidden" id="hidden_winner" name="winner">
-                    </form>
-                    <!--noch austauschen-->
+                    <div class="select_duelltwo">
+                        <div id="name02" class="txt_two">
+                            Name 02
+                        </div>
+                        <form id="hiddenForm" method="post">
+                            <input type="hidden" id="hidden_winner" name="winner">
+                        </form>
+                        <!--noch austauschen-->
+                    </div>
                 </div>
-            </div>
 
-            <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-            <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
-            <?php
+                <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+                <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+                <?php
          } else {
         echo "Bitte erst einloggen, <a href='login.php'>hier</a>.";
     }
         ?>
-        </body>
+            </body>
 
     </html>
--- win.php
+++ win.php
@@ -4,21 +4,23 @@
 	require_once("system/security.php");
 ?>
 
-<!DOCTYPE html>
-<html class="with-statusbar-overlay">
-   <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
-      <meta name="apple-mobile-web-app-capable" content="yes">
-      <meta name="apple-mobile-web-app-status-bar-style" content="black">
-      <title>Luisa - Die Namensapp</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/framework7/1.4.2/css/framework7.ios.min.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/framework7/1.4.2/css/framework7.ios.colors.min.css">
-      <link rel="stylesheet" href="css/style.css">
-   
-   </head>
-   <body>
-       <div class="section">
-           
+    <!DOCTYPE html>
+    <html class="with-statusbar-overlay">
+
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <title>Luisa - Die Namensapp</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/framework7/1.4.2/css/framework7.ios.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/framework7/1.4.2/css/framework7.ios.colors.min.css">
+        <link rel="stylesheet" href="css/style.css">
+
+    </head>
+
+    <body>
+        <div class="section">
+
             <div class="registrieren topbar">
                 <div class="txt_topbar">Duell beendet</div>
             </div>
@@ -28,7 +30,11 @@
             </div>
 
             <div class="heart_win">
-                <div class="winner">Luisa</div>
+                <div class="winner">
+                    <?php $name = $_GET["name"]; 
+                    echo $name;
+                    ?>
+                </div>
             </div>
 
             <div class="duell_bottom_einst">
@@ -38,22 +44,22 @@
             <div class="duell_bottom">
                 <input type="submit" class="submit" value="Endresultat">
             </div>
-        
-       </div> <!-- section -->
+
+        </div>
+        <!-- section -->
 
 
-      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/framework7/1.4.2/js/framework7.min.js"></script>
-      <script>
-         var myApp = new Framework7();
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/framework7/1.4.2/js/framework7.min.js"></script>
+        <script>
+            var myApp = new Framework7();
 
-         // Range Picker
-         var calendarRange = myApp.calendar({
-             input: '#calendar-range',
-             dateFormat: 'dd M yyyy',
-             rangePicker: true
-         });
+            // Range Picker
+            var calendarRange = myApp.calendar({
+                input: '#calendar-range',
+                dateFormat: 'dd M yyyy',
+                rangePicker: true
+            });
+        </script>
+    </body>
 
-
-      </script>
-   </body>
-</html>
+    </html>
