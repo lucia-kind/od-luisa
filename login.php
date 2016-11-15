5037c91791d44a4894330a8b1a670bbee9fd0b04
IsabelleSchwab <belle_schwab@hotmail.com>
2016-11-15 11:36:21 +0100

    login window.location.href

--- login.php
+++ login.php
@@ -43,7 +43,7 @@
          ?>
     <!--Weiterleitung Duell-->
     <script>
-        window.location.href = 'duell.php?duell_id=<?php echo $duell_id; ?>';
+        window.location = 'duell.php?duell_id=<?php echo $duell_id; ?>';
     </script>
     <?php
 
