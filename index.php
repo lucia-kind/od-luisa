e5365f8d85857d0fa930f9ccb6988096635b7674
IsabelleSchwab <belle_schwab@hotmail.com>
2016-11-16 16:58:52 +0100

    utf8

--- index.php
+++ index.php
@@ -7,9 +7,8 @@
     <html>
 
     <head>
-
         <title>Luisa - Die Namensapp</title>
-
+         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <link rel="stylesheet" href="css/style.css">
 
